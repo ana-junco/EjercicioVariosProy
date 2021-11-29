995873f2c747793647deb7c386dba13dd84fbc46
estudiante <estudiante@L-P1A-101>
2021-11-29 13:08:35 -0500

    Agregar archivos de proyecto.

---
+++ CallFunciones/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CallFunciones
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            BibliotecadeFunciones.Operaciones ob1 = new BibliotecadeFunciones.Operaciones();
+            double x1 = ob1.suma(289, 89, 100, 00);
+            double fac = ob1.factorial(5);
+            Console.WriteLine($"La suma es {5}");
+            Console.WriteLine($"El factorial de 5 es {fac}");
+            Console.WriteLine("Presiona center para finalizar...");
+            Console.ReadKey();
+        }
+       
+    }
+}
